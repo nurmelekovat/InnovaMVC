bf5cc533c6613c9e7fe235e1507a9daeec9d061b
nurmelekovat <nurmeleqovat@gmail.com>
2022-01-26 23:40:35 +0300

    Assosication=Classlar arasındaki zayıf bağlantı türü ile (loose coupling)

---
+++ src/main/java/com/patika/software/prensible/a1/assosication/AssosicationMainTest.java
@@ -0,0 +1,15 @@
+package com.patika.software.prensible.a1.assosication;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class AssosicationMainTest {
+    public static void main(String[] args) {
+        Musteri musteri=Musteri.builder().musteriAdi("Nurmelek").musteriSehir("Ankara").build();
+
+        SuperMarket superMarket=SuperMarket.builder().superMarketAdi("xsısdj").superMarketSehir("Ankara").build();
+
+        //loose Coupling (Zayıf bağlantı türüne ==> Association diyoruz
+        log.info("Müşteri adı " + musteri.getMusteriAdi() + " Süpermarket adi: " + superMarket.getSuperMarketAdi());
+    }
+}
