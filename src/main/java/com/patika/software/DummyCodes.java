eeca313cf7b91aadb09c3f558b996dbe00ca640b
nurmelekovat <nurmeleqovat@gmail.com>
2022-02-10 21:42:05 +0300

    Dummy Codes

---
+++ src/main/java/com/patika/software/DummyCodes.java
@@ -0,0 +1,5 @@
+package com.patika.software;
+
+public class DummyCodes {
+    //dummy codes: kullanılmayan veya kullanıolmayacak kodları  UI entegre edip boş yerine Ram v.b sistemi yormaya denir.
+}
